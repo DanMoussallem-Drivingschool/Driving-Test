5273b1cb028a02b2fbe8e659f827721a671a7815
DanMoussallem-Drivingschool <joemoussallem@gmail.com>
2026-01-22 09:54:31 +0200

    Update signs_data.js

--- tests/signs_data.js
+++ tests/signs_data.js
@@ -1,1 +1,36 @@
+// signs_data.js
+// extracted from Lebanese traffic signs PDFs
+
+window.TRAFFIC_SIGNS = [
+
+  {
+    id: 1,
+    name_ar: "ممنوع الدخول",
+    desc_ar: "يمنع دخول جميع المركبات من هذا الاتجاه.",
+    category_ar: "إشارات المنع",
+    image: "../assets/signs/no_entry.png"
+  },
+  {
+    id: 2,
+    name_ar: "قف",
+    desc_ar: "يجب التوقف الكامل قبل المتابعة.",
+    category_ar: "إشارات تنظيمية",
+    image: "../assets/signs/stop.png"
+  },
+  {
+    id: 3,
+    name_ar: "طريق رئيسي",
+    desc_ar: "أنت تسير على طريق له أولوية المرور.",
+    category_ar: "إشارات إرشادية",
+    image: "../assets/signs/main_road.png"
+  },
+  {
+    id: 4,
+    name_ar: "ممر مشاة",
+    desc_ar: "وجود ممر لعبور المشاة.",
+    category_ar: "إشارات تحذيرية",
+    image: "../assets/signs/pedestrian.png"
+  }
+
+];
 
