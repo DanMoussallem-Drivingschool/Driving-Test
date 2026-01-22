d789668b35a8cb7481d545e5bf7d7e50142b96af
DanMoussallem-Drivingschool <joemoussallem@gmail.com>
2026-01-22 13:25:07 +0200

    Update signs_data.js

--- tests/signs_data.js
+++ tests/signs_data.js
@@ -8,28 +8,28 @@ window.TRAFFIC_SIGNS = [
     name_ar: "ممنوع الدخول",
     desc_ar: "يمنع دخول جميع المركبات من هذا الاتجاه.",
     category_ar: "إشارات المنع",
-    image: "../assets/signs/no_entry.png"
+    image: "../assets/signs/no_entry.webp"
   },
   {
     id: 2,
     name_ar: "قف",
     desc_ar: "يجب التوقف الكامل قبل المتابعة.",
     category_ar: "إشارات تنظيمية",
-    image: "../assets/signs/stop.png"
+    image: "../assets/signs/stop.webp"
   },
   {
     id: 3,
     name_ar: "طريق رئيسي",
     desc_ar: "أنت تسير على طريق له أولوية المرور.",
     category_ar: "إشارات إرشادية",
-    image: "../assets/signs/main_road.png"
+    image: "../assets/signs/main_road.webp"
   },
   {
     id: 4,
     name_ar: "ممر مشاة",
     desc_ar: "وجود ممر لعبور المشاة.",
     category_ar: "إشارات تحذيرية",
-    image: "../assets/signs/pedestrian.png"
+    image: "../assets/signs/pedestrian.webp"
   }
 
 ];
